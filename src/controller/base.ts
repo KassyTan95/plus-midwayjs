65d3577e975aa0d5f6b273ca675342edaba67382
Kassy <kassyt@126.com>
2024-03-25 21:55:40 +0800

    common controller add method

--- src/controller/base.ts
+++ src/controller/base.ts
@@ -3,6 +3,8 @@ import { Context } from '@midwayjs/koa'
 import * as dayjs from 'dayjs'
 import { RESCODE } from '../enums/global'
 import type { BaseResponse } from '../interface'
+import * as jwt from 'jsonwebtoken'
+import { ForbiddenError } from '@midwayjs/core/dist/error/http'
 
 @Provide()
 export abstract class BaseController {
@@ -33,5 +35,18 @@ export abstract class BaseController {
     }
     isShowTime && (result['timestamp'] = dayjs().unix())
     return result
+  }
+
+  /**
+   * 解析 token
+   * @param header
+   * @returns
+   */
+  protected deToken(header: string) {
+    const token = header.trim().split(' ')[1]
+    if (!token) {
+      throw new ForbiddenError('请先登录')
+    }
+    return jwt.decode(token, { complete: true }).payload
   }
 }
